ace14cc2b838f11ffcd82c7336e963a48141027d
dmitriy <jaddressbook@gmail.com>
2021-11-05 18:19:41 -0700

    woork on getting new links from phones

--- src/java/com/walletwizz/sharelinks/ux/Sync.java
+++ src/java/com/walletwizz/sharelinks/ux/Sync.java
@@ -1,15 +1,19 @@
 package com.walletwizz.sharelinks.ux;
 
 import java.util.stream.Stream;
+//import java.util.stream.builder;
 import java.io.InputStreamReader;
 import javax.json.JsonReader;
 import javax.json.JsonArray;
 import javax.json.Json;
 
 import org.aldan3.util.DataConv;
+import org.aldan3.data.DODelegator;
+import org.aldan3.model.ProcessException;
 
 import com.beegman.webbee.block.Conversational;
 import com.walletwizz.sharelinks.model.link;
+
 import com.walletwizz.sharelinks.model.SharelinksModel;
 
 public class Sync extends Conversational<Stream<link>, Stream<link>, SharelinksModel> {
@@ -19,16 +23,36 @@ public class Sync extends Conversational<Stream<link>, Stream<link>, SharelinksM
 		}
 	
 	protected Stream<link> readModel() {
+		Stream.Builder<link> builder = Stream.builder();
 		try( JsonReader jsonReader =  Json.createReader(
 				new InputStreamReader(req.getInputStream(), DataConv.ifNull(getEncoding(), "utf-8")))) {
 			JsonArray array = jsonReader.readArray();
-			link[] links = new link[array.size()];
 			
-			return Stream.of(links);
+			 for (int i = 0, size = array.size(); i < size; i++)
+			    {
+			      builder.accept((link)fillPojo(new link(getAppModel()), array.getJsonObject(i)));
+			    }
+			
+		
 		} catch(Exception e) {
 			
 		}
-		return Stream.of();
+		return builder.build();
 	}
+	
+	protected Stream<link> process(Stream<link> ask) {
+		ask.forEach(l -> {
+			DODelegator<link> sdo1 = new DODelegator<>(l);
+			try {
+				if(l.id == 0) {
+					getAppModel().getDOService().addObject(sdo1);
+				}
+			} catch (ProcessException pe) {
+				log("", pe);
+			}
+		});
+		
+		return (Stream.<link>builder()).build();	
+		}
 
 }
