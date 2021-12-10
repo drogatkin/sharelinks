508d9344209d99905ab50fb0ba9e267a809a60cc
dmitriy <jaddressbook@gmail.com>
2021-12-09 21:21:33 -0800

    custom function on open

--- src/js/wsinit.js
+++ src/js/wsinit.js
@@ -1,7 +1,14 @@
 function wsinit(ur) {
+	//console.log('WSAPI initialized as '+ WSAPI)
+	// alert('extra for '+ctx+' and '+WSAPI)
 	// TODO change parameters to pass context name
-	WSAPI.init(ur, "refresh")
+	WSAPI.init(ur, "refresh", sub_to_refresh)
 //	WSAPI.addListener('newdata', updatelist)
+	
+}
+
+function sub_to_refresh() {
+//	alert('conn op')
 	WSAPI.subscribe('refresh')
 }
 
